965958ee91024a22019b4fc80985465e71168246
Vivek kumar singh <167025814+vivek-ksinghg@users.noreply.github.com>
2025-02-28 12:16:37 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code an newer version
+
+ // alert(3+3) // we are using nodejs , not browser
+
+ // number=> 2 to power 53
+ // bigint
+ // string =>""
+ // boolean=> true or false
+ // null=> standalone value
+ // undefined =>
+ // symbol=> unique
+
+ 
+
+console.log(3+3);
