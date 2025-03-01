b38f08190d8519685b68654a58520c8829ed812e
Vivek kumar singh <167025814+vivek-ksinghg@users.noreply.github.com>
2025-03-01 07:46:23 +0000

    string

---
+++ 01_basics/05_String.js
@@ -0,0 +1,35 @@
+const name="vivek";
+const repoCount=50;
+
+//console.log(name+repoCount);
+
+//console.log(`my name is ${name} and my repository count is ${repoCount}`);
+
+const getName=new String('aarnaw');
+ // console.log(getName);
+
+// console.log(getName[0]);
+// console.log(getName._proto_);
+
+// console.log(getName.length);
+// console.log(getName.toUpperCase());
+// console.log(getName.charAt(2));
+// console.log(getName.indexOf('w'));
+
+
+// const newString=getName.substring(2,5);
+// console.log(newString);
+
+// const anotherStrinng=getName.slice(2,5);
+// console.log("slice="+anotherStrinng);
+
+// const newStringOne="    ram   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const lergeNmae="singh kumar vivek";
+console.log(lergeNmae);
+console.log(lergeNmae.replaceAll(' ','$'));
+
+
