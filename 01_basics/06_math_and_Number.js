d2f69da3ad8aedabdf543bf65f2b2f4e4d351d90
Vivek kumar singh <167025814+vivek-ksinghg@users.noreply.github.com>
2025-03-01 08:40:03 +0000

    Math and Number

---
+++ 01_basics/06_math_and_Number.js
@@ -0,0 +1,25 @@
+// const num=new Number(100);
+// console.log(num);
+
+// console.log(num.toString().length);
+// console.log(num.toFixed(1));
+
+//   const otherNumber=23.865;
+//   console.log(otherNumber.toPrecision(9));
+  
+//   const hundred=100000;
+//   console.log(hundred.toLocaleString('en-In'));
+
+// *************MATH**********
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+
+
+
+
