396be12163326ceb0eb8e9e940ad4eac4da8de38
Vivek kumar singh <167025814+vivek-ksinghg@users.noreply.github.com>
2025-03-02 10:08:42 +0000

    Date in js

---
+++ 01_basics/07_date.js
@@ -0,0 +1,17 @@
+// Date 
+
+// let myDate=new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+// let myCreateDate=new Date(2025,2,2);
+
+// console.log(myCreateDate.toDateString());
+
+// TIME STAMP
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
