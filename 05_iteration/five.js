ab87d7d7f84a8eafd76f50689c9980babdbc5dba
Vivek kumar singh <167025814+vivek-ksinghg@users.noreply.github.com>
2025-03-08 14:48:53 +0000

    loop discussion

---
+++ 05_iteration/five.js
@@ -0,0 +1,47 @@
+const coding=["js","python","java","c++","c"];
+
+// coding.forEach(function(val){
+//     console.log(val);
+    
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+    
+// })
+
+const myCoding=[
+    {
+        languageName:"js",
+        languageFileName:"javaScript"
+    },
+    {
+        languageName:"java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+
+];
+
+myCoding.forEach((item)=>{
+    
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+    
+    
+})
---
+++ 05_iteration/four.js
@@ -0,0 +1,19 @@
+const myobj={
+    name:"ram",
+    id:123,
+    branch:"computer science",
+    section:"cs-A1"
+}
+
+for(key in myobj){
+  console.log(`${key} of student college ${myobj[key]}`);    
+}
+
+const programming=["js","java","c++","html"];
+for(const key in programming){
+  
+  console.log(programming[key]);
+  
+}
+
+
